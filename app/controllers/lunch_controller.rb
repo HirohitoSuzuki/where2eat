d52e0c8ad075be169238f35ccbf43f0fbf8aa8f7
Hirohito Suzuki <s1913565@s.tsukuba.ac.jp>
2019-05-10 15:58:16 +0900

    店をベタ打ちして数を増やした。

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,4 +1,6 @@
 class LunchController < ApplicationController
     def go
+        eataries = ['松乃家','松のや','松屋','半田屋','あすかて食堂','te.toS.te','トタンコットンカフェ','711で弁当']
+        @recommend = eataries.sample
     end
 end
