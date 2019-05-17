39148c3b5c12891fd8b2481970c434230f102e9c
Hirohito Suzuki <s1913565@s.tsukuba.ac.jp>
2019-05-17 17:45:34 +0900

    ログイン機能をつけた

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'hateries/new'
-  get 'hate_eateries/new'
   get 'eateries/new'
   get 'lunch/go',to:'lunch#go'
   root 'lunch#go'
-  post 'hate_eateries/create'
   post 'hateries/create'
+  get '/login', to:'sessions#new'
+  post 'sessions/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
